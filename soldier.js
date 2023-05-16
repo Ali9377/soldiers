9202a4cb89f779bed56cb727c6f59f1ac5e037e2
ali <loading.03@mail.ru>
2023-05-16 14:56:39 +0300

    Закончил делать задание

--- soldier.js
+++ soldier.js
@@ -6,8 +6,30 @@ const soldier = {
         numberСartridges: 35,
     },
     supplies: 3,
-    
-    shoot: function() {},
-    rechange: function() {},
-    to_hurt: function() {},
+
+    shoot: function() {
+        if(soldier.weapon.numberСartridges <= 0) {
+            console.log('Обойма пуста. Перезарядитесь') 
+        } else { 
+        this.weapon.numberСartridges--
+            console.log('Бах-бах!') 
+        }
+    },
+    rechange: function() {
+        if(soldier.supplies <= 0) {
+            console.log('не осталось припасов') 
+        } else {
+            console.log('перезарядка...') 
+            soldier.weapon.numberСartridges = 30
+            this.supplies--
+            
+        }
+        
+    },
+    to_hurt: function() {
+        this.health--
+        if(soldier.health <= 0) {
+            console.log('Ты проиграл')
+        }
+    },
 }
