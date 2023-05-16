ec219deae8f904b101a1e5171824fb68c4aae396
ali <loading.03@mail.ru>
2023-05-16 14:33:39 +0300

    Создал персонажа

---
+++ soldier.js
@@ -0,0 +1,13 @@
+const soldier = {
+    soldierName: 'John',
+    health: 10,
+    weapon: {
+        weaponName: 'Pistolet',
+        numberСartridges: 35,
+    },
+    supplies: 3,
+    
+    shoot: function() {},
+    rechange: function() {},
+    to_hurt: function() {},
+}
